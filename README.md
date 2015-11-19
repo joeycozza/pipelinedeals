d0818870bb844c7e07b41a833e9d225f403109ac
Joey Cozza <joeycozza@gmail.com>
2015-11-19 11:46:28 -0700

    initial commit, gets and pagination works

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # pipelinedeals
-A node module for easier access to the PipelineDeals API.
+A node module for easier access to the GET calls of the PipelineDeals API. 
+This essentially is just a wrapper around the pagination that they implement.
 
-This module handles pagination automatically.
+##Usage
+```
+var pld = require('pipelinedeals');
+pld.get({endpoint: 'deals.json', apiKey: 'INSERT_API_KEY_HERE'}, function(err, entries, allInfo){
+  //here you have access to the data from the call
+});
+```
+
+Options should appear as such
+```javascript
+{
+  endpoint: STRING, //REQUIRED
+  apiKey: STRING //REQUIRED
+}
+```
+
+The endpoint for this url `https://api.pipelinedeals.com/api/v3/deals.json` is `deals.json`
+The endpoint for this url `https://api.pipelinedeals.com/api/v3/account_notifications.json` is `account_notifications.json`
---
+++ index.js
@@ -0,0 +1,40 @@
+var request  = require('request');
+var BASE_URL = 'https://api.pipelinedeals.com/api/v3/';
+
+module.exports = {
+
+  get: function (options, callback) {
+    if (!options.endpoint || !options.apiKey) {
+      callback('options.endpoint and options.apiKey are both required');
+    }
+
+    options.endpoint = options.endpoint[0] === '/' ? options.endpoint.slice(1) : options.endpoint;
+    var allEntries   = [];
+
+    function makeCall(page) {
+
+      var url = BASE_URL + options.endpoint + '?api_key=' + options.apiKey + '&page=' + page + '&per_page=200';
+
+      request({url: url}, function (err, response, body) {
+        if (err) {
+          return callback(err);
+        }
+        try {
+          body = JSON.parse(body);
+        } catch (e) {
+          return callback('Response was not valid json. The Url was probably invalid. URL: ' + url);
+        }
+
+        allEntries = allEntries.concat(body.entries);
+
+        if (body.pagination.page < body.pagination.pages) {
+          makeCall(++page);
+        } else {
+          callback(null, allEntries);
+        }
+      });
+    }
+
+    makeCall(1);
+  }
+};
